8cd6dcd7d8fcc478c5bfad3acc4f1aa9fbb355eb
Radosław Bednarczyk <radek-bednarczyk@live.com>
2026-04-16 19:48:26 +0200

    pragma pack() exmaple

--- sizeof_unique_ptr_vs_shared_ptr.cpp
+++ sizeof_unique_ptr_vs_shared_ptr.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+#pragma pack(1)
+class Foo
+{
+    char m_char;
+    int m_value;
+};
+#pragma pack()
+
 int main()
 {
     std::unique_ptr<int> ptr1 = std::make_unique<int>(42);
@@ -7,6 +15,14 @@ int main()
 
     std::cout << std::endl << sizeof(ptr1); // 8 bytes (size of unique_ptr)
     std::cout << std::endl << sizeof(ptr2); // 16 bytes (size of shared_ptr)
+    std::cout << std::endl << sizeof(int); // 4 bytes (size of int)
+    std::cout << std::endl << sizeof(int*); // 8 bytes (size of int*)4
+    std::cout << std::endl << sizeof(char); // 1 byte
+    std::cout << std::endl << sizeof(char*); // 8 bytes
+
+    std::cout << std::endl << sizeof(Foo); // 5 byte
+    std::cout << std::endl << sizeof(Foo*); // 8 bytes
+
 
     return 0;
 }
